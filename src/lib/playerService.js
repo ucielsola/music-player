40fc245a3c011268ce32a7b0da6d9e4e60e61797
Uciel Sola <solauciel@gmail.com>
2022-05-08 01:03:45 -0300

    basic player functions working

--- src/lib/playerService.js
+++ src/lib/playerService.js
@@ -1,20 +1,93 @@
-import { readable, writable, get } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 import { album } from './contentService';
 
-const track = (url) => {
-	return new Audio(url);
+let audio;
+
+let _album;
+
+let canResume = false;
+
+const events = [
+	'ended',
+	'error',
+	'play',
+	'playing',
+	'pause',
+	'timeupdate',
+	'canplay',
+	'canplaythrough',
+	'loadedmetadata'
+];
+
+const addAudioEvents = () => {
+	events.forEach((ev) => {
+		audio.addEventListener(ev, handlers);
+	});
 };
 
-export const playing = readable(false, (set) => {
-	set(track);
-	return () => {};
-});
+const handlers = (e) => {
+	switch (e.type) {
+		case 'ended':
+			if (_album.songs.length === get(currentTrack)) return;
+			Play(get(currentTrack) + 1);
+			playing.set(true);
+			break;
+		case 'error':
+			playing.set(false);
+			break;
+		case 'play':
+			break;
+		case 'playing':
+			playing.set(true);
+			break;
+		case 'pause':
+			canResume = true;
+			playing.set(false);
+			break;
+		case 'timeupdate':
+			currentTime.set(audio.currentTime * 1000);
+			console.log(audio.currentTime * 1000);
+			break;
+		case 'canplay':
+			break;
+	}
+};
 
-export const Play = (trackNum) => {
-	let _album = get(album);
+export const createContext = () => {
+	_album = get(album);
+	audio = new Audio();
+	addAudioEvents();
 
-	const url = _album.songs[trackNum].file.url;
-	track(url)
-		.play()
-		.then(() => {});
+	document.addEventListener('keydown', (e) => {
+		if (e.code === 'Space' && get(playing)) {
+			return Pause();
+		} else {
+			return Play();
+		}
+	});
+};
+
+export const playing = writable(false);
+
+export const currentTrack = writable(false);
+
+export const currentTime = writable(0);
+
+export const trackLength = writable(0);
+
+export const Play = (trackNum = 0) => {
+	if (canResume && trackNum === get(currentTrack)) {
+		canResume = false;
+		return audio.play();
+	}
+
+	audio.src = _album.songs[trackNum].file.url;
+	audio.crossOrigin = 'anonymous';
+	audio.load();
+	trackLength.set(audio.duration);
+	audio.play().then(() => currentTrack.set(trackNum));
+};
+
+export const Pause = () => {
+	audio.pause();
 };
