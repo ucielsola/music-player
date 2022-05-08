4698bee60af11bf89e96b73857e9b5e17cb7c357
Uciel Sola <solauciel@gmail.com>
2022-05-08 02:03:26 -0300

    tracklist cleanup

--- src/lib/components/tracklist.svelte
+++ src/lib/components/tracklist.svelte
@@ -1,16 +1,14 @@
 <script>
-	import { Play } from '$lib/playerService.js';
+	import { Play, currentTrack } from '$lib/playerService.js';
 	export let tracks;
-	let currentTrack = 5;
 </script>
 
 <div class="container">
 	{#if tracks}
 		{#each tracks as track, i}
-			<h3 id={i} class={i === currentTrack ? 'active' : ''} on:click={() => Play(i)}>
+			<h3 id={i} class={i === $currentTrack ? 'active' : ''} on:click={() => Play(i)}>
 				{'0' + (i + 1)} - {track.title}
 			</h3>
-			<span>{track.file.url}</span>
 		{/each}
 	{/if}
 </div>
