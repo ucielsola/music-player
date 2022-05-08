102962a2d9dcd0e479a9e929371755b5d78e37ef
Uciel Sola <solauciel@gmail.com>
2022-05-08 16:24:45 -0300

    player controls bounce entrance

--- src/lib/components/playerControls.svelte
+++ src/lib/components/playerControls.svelte
@@ -15,7 +15,7 @@
 	};
 </script>
 
-<div class="container">
+<div class="container slide-in-bottom">
 	<TrackButtons on:pause={() => Pause()} on:play={() => Play()} playing={$playing} />
 	<ProgressBar
 		on:updateTime={handleSeek}
@@ -38,5 +38,35 @@
 		background-color: var(--contrast);
 		border-top: 1px solid hsl(120deg 33% 1% / 77%);
 		box-shadow: var(--contrast) 0px 16px 50px 12px;
+	}
+
+	.slide-in-bottom {
+		-webkit-animation: slide-in-bottom 0.5s cubic-bezier(0.38, 1.01, 0.28, 1.01) both 0.5s;
+		animation: slide-in-bottom 0.5s cubic-bezier(0.38, 1.01, 0.28, 1.01) both 0.5s;
+	}
+
+	@-webkit-keyframes slide-in-bottom {
+		0% {
+			-webkit-transform: translateY(1000px);
+			transform: translateY(1000px);
+			opacity: 0;
+		}
+		100% {
+			-webkit-transform: translateY(0);
+			transform: translateY(0);
+			opacity: 1;
+		}
+	}
+	@keyframes slide-in-bottom {
+		0% {
+			-webkit-transform: translateY(1000px);
+			transform: translateY(1000px);
+			opacity: 0;
+		}
+		100% {
+			-webkit-transform: translateY(0);
+			transform: translateY(0);
+			opacity: 1;
+		}
 	}
 </style>
