dbe6d10f0a84d93e2e19402851d68200d1dd45d5
Uciel Sola <solauciel@gmail.com>
2022-05-08 02:04:19 -0300

    added some new structure

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,18 @@
+<script>
+	import CoverSlider from './coverSlider.svelte';
+
+	export let cover;
+	export let back;
+	export let artist;
+	export let title;
+</script>
+
+<div class="container">
+	<CoverSlider images={[cover, back]} altText="{artist} - {title}" />
+</div>
+
+<style>
+	.container {
+		position: relative;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,29 @@
 <script>
 	import { album } from '$lib/contentService.js';
-	import { playing } from '$lib/playerService.js';
+	import { initPlayer, playing } from '$lib/playerService.js';
 	import Controls from '../lib/components/controls.svelte';
 	import Tracklist from '../lib/components/tracklist.svelte';
+	import { browser } from '$app/env';
+	import Header from '../lib/components/header.svelte';
+
+	$: if ($album != 'loading' && browser) initPlayer();
 </script>
 
 <main>
 	{#await $album then}
+		<Header artist={$album.artist} cover={$album.cover} back={$album.back} title={$album.title} />
 		<Tracklist tracks={$album.songs} />
-		{$playing}
 		<Controls />
 	{/await}
 </main>
 
 <style>
 	main {
-		position: relative;
+		/* position: relative; */
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
+		padding-bottom: calc(1rem + var(--controls-height));
 		background-color: var(--background);
 	}
 </style>
