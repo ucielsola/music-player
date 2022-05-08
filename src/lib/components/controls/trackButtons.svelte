917e029c62d92f26d2724c3e409f61ae6f11b812
Uciel Sola <solauciel@gmail.com>
2022-05-08 16:58:51 -0300

    added next prev buttons

--- src/lib/components/controls/trackButtons.svelte
+++ src/lib/components/controls/trackButtons.svelte
@@ -9,41 +9,59 @@
 	}
 </script>
 
-{#if playing}
-	<button on:click={() => handleClick('pause')} title="Pause">
-		<img class="pause" src="assets/icons/pause.png" alt="" />
+<div class="container">
+	<button class="small" on:click={() => handleClick('prev')} title="Previous">
+		<img class="prev" src="assets/icons/previous.png" alt="" />
 	</button>
-{:else}
-	<button on:click={() => handleClick('play')} title="Play">
-		<img class="play" src="assets/icons/play.png" alt="" />
+	{#if playing}
+		<button class="play-pause" on:click={() => handleClick('pause')} title="Pause">
+			<img class="pause" src="assets/icons/pause.png" alt="" />
+		</button>
+	{:else}
+		<button class="play-pause" on:click={() => handleClick('play')} title="Play">
+			<img class="play" src="assets/icons/play.png" alt="" />
+		</button>
+	{/if}
+	<button class="small" on:click={() => handleClick('next')} title="Next">
+		<img class="next" src="assets/icons/next.png" alt="" />
 	</button>
-{/if}
+</div>
 
 <style>
 	button {
 		border: none;
 		background: none;
 		outline: none;
+	}
+
+	.container {
+		display: flex;
+	}
+
+	.small {
+		width: 2rem;
+	}
+	.play-pause {
 		display: grid;
 		place-content: center;
 		background-color: var(--primary);
-		width: 3rem;
+		width: 2.5rem;
 		aspect-ratio: 1/1;
 		border-radius: 100%;
 		box-shadow: var(--shadow);
 		transition: all 0.1s ease-in-out;
 	}
 
-	button:hover,
-	button:active {
-		transform: scale(1.3);
+	.play-pause:active,
+	.play-pause:focus {
+		transform: scale(0.9);
 		transition: all 0.1s ease-in-out;
 	}
 
-	button img {
+	.play-pause img {
 		width: 2rem;
 	}
 	img.play {
-		padding-left: 0.25rem;
+		padding-left: 0.15rem;
 	}
 </style>
