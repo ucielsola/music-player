d514f15b15ebd9e3f5b2a89d93d50368533125ca
Uciel Sola <solauciel@gmail.com>
2023-08-20 20:54:48 -0300

    feat: tooltips + remove unused

--- src/lib/components/controls/PlayerButtons.svelte
+++ src/lib/components/controls/PlayerButtons.svelte
@@ -12,11 +12,19 @@
 </script>
 
 <div class="flex items-center gap-4 text-stone-50">
-	<button on:click={playPrevTrack} class="transition-colors duration-75 active:text-stone-500">
+	<button
+		on:click={playPrevTrack}
+		class="transition-colors duration-75 active:text-stone-500"
+		title="Previous Track"
+	>
 		<Icon icon="pepicons-pop:previous-track-circle" class="w-8 h-8" />
 	</button>
 
-	<button on:click={togglePlay} class="transition-colors duration-75 active:text-stone-500">
+	<button
+		on:click={togglePlay}
+		class="transition-colors duration-75 active:text-stone-500"
+		title={$playerStore.controls.isPlaying ? 'Pause' : 'Play'}
+	>
 		{#if $playerStore.controls.isPlaying}
 			<Icon icon="pepicons-pop:pause-circle" class="w-12 h-12" />
 		{:else}
@@ -24,13 +32,21 @@
 		{/if}
 	</button>
 
-	<button on:click={playNextTrack} class="transition-colors duration-75 active:text-stone-500">
+	<button
+		on:click={playNextTrack}
+		class="transition-colors duration-75 active:text-stone-500"
+		title="Next Track"
+	>
 		<Icon icon="pepicons-pop:next-track-circle" class="w-8 h-8" />
 	</button>
 </div>
 
 <div class="flex items-center gap-8">
-	<button on:click={toggleRepeat} class="transition-colors duration-75 active:text-stone-500">
+	<button
+		on:click={toggleRepeat}
+		class="transition-colors duration-75 active:text-stone-500"
+		title={`Repeat: ${!$playerStore.controls.isRepeat ? 'on' : 'off'}`}
+	>
 		{#if $playerStore.controls.isRepeat}
 			<Icon icon="pepicons-pop:repeat-circle-off" class="w-6 h-6 text-stone-50" />
 		{:else}
@@ -38,7 +54,11 @@
 		{/if}
 	</button>
 
-	<button on:click={toggleShuffle} class="transition-colors duration-75 active:text-stone-500">
+	<button
+		on:click={toggleShuffle}
+		class="transition-colors duration-75 active:text-stone-500"
+		title={`Shuffle: ${!$playerStore.controls.isShuffle ? 'on' : 'off'}`}
+	>
 		{#if $playerStore.controls.isShuffle}
 			<Icon icon="pepicons-pop:list-circle-off" class="w-6 h-6 text-stone-50" />
 		{:else}
