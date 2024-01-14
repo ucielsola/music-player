e58b373a369fd2ccca5479595fa28b04f29d5394
Uciel Sola <soluciel@gmail.com>
2024-01-14 16:12:44 -0300

    prevet button spamming

--- src/lib/components/controls/PlayerButtons.svelte
+++ src/lib/components/controls/PlayerButtons.svelte
@@ -10,21 +10,48 @@
 		toggleShuffle
 	} from '$lib/player/actions';
 	import { playerStore } from '$lib/player/playerStore';
+
+	let btnsDisabled = false;
+	let lastButtonClicked: () => void;
+
+	function handleButtonClick(action: () => void) {
+		if (lastButtonClicked !== action) {
+			lastButtonClicked = action;
+			action();
+
+			return;
+		}
+
+		if (!btnsDisabled) {
+			// Disable the button
+			btnsDisabled = true;
+
+			// Perform the button action
+			action();
+
+			// Enable the button after a delay (e.g., 2 seconds)
+			setTimeout(() => {
+				btnsDisabled = false;
+			}, 800);
+		}
+	}
 </script>
 
 <div class="flex items-center gap-4 text-stone-50">
 	<button
-		on:click={playPrevTrack}
+		on:click={() => handleButtonClick(playPrevTrack)}
 		class="transition-colors duration-75 active:text-stone-500"
 		title={$LL.prev()}
+		disabled={btnsDisabled}
 	>
 		<Icon icon="pepicons-pop:previous-track-circle" class="w-8 h-8" />
 	</button>
 
 	<button
-		on:click={togglePlay}
+		on:click={() => handleButtonClick(togglePlay)}
 		class="text-xl transition-colors duration-75 active:text-stone-500"
 		title={$playerStore.controls.isPlaying ? $LL.play() : $LL.play()}
+		disabled={btnsDisabled}
 	>
 		{#if $playerStore.controls.isPlaying}
 			<Icon icon="pepicons-pop:pause-circle" class="w-12 h-12" />
@@ -34,9 +61,10 @@
 	</button>
 
 	<button
-		on:click={playNextTrack}
+		on:click={() => handleButtonClick(playNextTrack)}
 		class="transition-colors duration-75 active:text-stone-500"
 		title={$LL.next()}
+		disabled={btnsDisabled}
 	>
 		<Icon icon="pepicons-pop:next-track-circle" class="w-8 h-8" />
 	</button>
@@ -44,9 +72,10 @@
 
 <div class="flex items-center gap-8">
 	<button
-		on:click={toggleRepeat}
+		on:click={() => handleButtonClick(toggleRepeat)}
 		class="transition-colors duration-75 active:text-stone-500"
 		title={`${$LL.repeat()}: ${$playerStore.controls.isRepeat ? $LL.on() : $LL.off()}`}
+		disabled={btnsDisabled}
 	>
 		{#if $playerStore.controls.isRepeat}
 			<Icon icon="pepicons-pop:repeat-circle-off" class="w-6 h-6 text-stone-50" />
@@ -56,9 +85,10 @@
 	</button>
 
 	<button
-		on:click={toggleShuffle}
+		on:click={() => handleButtonClick(toggleShuffle)}
 		class="transition-colors duration-75 active:text-stone-500"
 		title={`${$LL.shuffle()}: ${$playerStore.controls.isShuffle ? $LL.on() : $LL.off()}`}
+		disabled={btnsDisabled}
 	>
 		{#if $playerStore.controls.isShuffle}
 			<Icon icon="pepicons-pop:list-circle-off" class="w-6 h-6 text-stone-50" />
