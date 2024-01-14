1c1ae3735333bd27a9801609687a4e821bd6768e
Uciel Sola <soluciel@gmail.com>
2024-01-14 14:03:30 -0300

    prevent memory leak

--- src/lib/components/CoverSlideshow.svelte
+++ src/lib/components/CoverSlideshow.svelte
@@ -1,44 +1,31 @@
 <script lang="ts">
+	import { onDestroy } from 'svelte';
 	import { fade } from 'svelte/transition';
+
 	import { playerStore } from '$lib/player/playerStore';
 
 	import Cover from '$lib/components/Cover.svelte';
 
-	import type { Album, CoverImage } from '$lib/interfaces';
+	import type { CoverImage } from '$lib/interfaces';
 
 	const INTERVAL_DURATION = 15000;
 	let showCover = false;
 	let images: CoverImage[] = [];
+	let intervalId: number;
 
 	$: currentImage = images[Number(showCover)];
 
 	$: if ($playerStore.loaded && $playerStore.album) {
-		images = createImagesArray($playerStore.album);
+		images = $playerStore.album.images.sort((a, b) => a.position - b.position);
 	}
 
-	const createImagesArray = (album?: Album): CoverImage[] => {
-		const images: CoverImage[] = [];
-
-		if (album) {
-			const cover: CoverImage = {
-				url: album.cover,
-				placeholderUrl: album.coverPlaceholder
-			};
-
-			const back: CoverImage = {
-				url: album.back,
-				placeholderUrl: album.backPlaceholder
-			};
-
-			images.push(cover, back);
-		}
-
-		return images;
-	};
-
-	setInterval(() => {
+	intervalId = setInterval(() => {
 		showCover = !showCover;
 	}, INTERVAL_DURATION);
+
+	onDestroy(() => {
+		clearInterval(intervalId);
+	});
 </script>
 
 <div in:fade|global>
