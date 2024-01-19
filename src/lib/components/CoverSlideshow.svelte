0927c78d88c013537344e7d7e2597b2eff62e4dc
Uciel Sola <solauciel@gmail.com>
2024-01-19 13:01:56 -0300

    Update CoverSlideshow - prevent image switch if only one

--- src/lib/components/CoverSlideshow.svelte
+++ src/lib/components/CoverSlideshow.svelte
@@ -1,31 +1,46 @@
 <script lang="ts">
-	import { onDestroy } from 'svelte';
-	import { fade } from 'svelte/transition';
+import {
+    onDestroy
+} from 'svelte';
+import {
+    fade
+} from 'svelte/transition';
 
-	import { playerStore } from '$lib/player/playerStore';
+import {
+    playerStore
+} from '$lib/player/playerStore';
 
-	import Cover from '$lib/components/Cover.svelte';
+import Cover from '$lib/components/Cover.svelte';
 
-	import type { CoverImage } from '$lib/interfaces';
+import type {
+    CoverImage
+} from '$lib/interfaces';
 
-	const INTERVAL_DURATION = 15000;
-	let showCover = false;
-	let images: CoverImage[] = [];
-	let intervalId: number;
+const INTERVAL_DURATION = 15000;
+let showCover = false;
+let images: CoverImage[] = [];
+let intervalId: number;
 
-	$: currentImage = images[Number(showCover)];
+$: currentImage = images.length > 1 ? images[Number(showCover)] : images[0]
 
-	$: if ($playerStore.loaded && $playerStore.album) {
-		images = $playerStore.album.images.sort((a, b) => a.position - b.position);
-	}
+$: if ($playerStore.loaded && $playerStore.album) {
+    images = $playerStore.album.images.sort((a, b) => a.position - b
+        .position);
+}
 
-	intervalId = setInterval(() => {
-		showCover = !showCover;
-	}, INTERVAL_DURATION);
+$: if (intervalId === undefined && images.length > 1) {
+    intervalId = createInterval()
+}
 
-	onDestroy(() => {
-		clearInterval(intervalId);
-	});
+function createInterval() {
+    return setInterval(() => {
+        showCover = !showCover;
+    }, INTERVAL_DURATION);
+}
+
+onDestroy(() => {
+    clearInterval(intervalId);
+});
 </script>
 
 <div in:fade|global>
